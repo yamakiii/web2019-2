42d0ed505bb9a6ab22951b67592f3726422b4b5d
zm16555 <zm16555@josai.ac.jp>
2019-01-20 20:53:17 -0500

    message

---
+++ ex0-1.js
@@ -0,0 +1,16 @@
+ function fruit1(){
+  document.getElementById('myImage').src='./image/budou.png';}
+
+function fruit2(){
+  document.getElementById('myImage').src='./image/mikan.png';}
+
+function fruit3(){
+  document.getElementById('myImage').src='./image/melon.png';}
+
+
+
+
+  
+ 
+  
+ 
